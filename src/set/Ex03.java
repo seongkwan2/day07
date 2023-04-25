7ae20be3c4861c4bccacf7c65e5b2b1dc84a66aa
test123 <zaq3195@naver.com>
2023-04-25 11:28:29 +0900

    2023-04-25수정

--- src/set/Ex03.java
+++ src/set/Ex03.java
@@ -7,13 +7,16 @@ public class Ex03 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		HashSet<String> name = new HashSet<>();
+		
 		String n;
 		while(true) {
 			System.out.println("이름 입력: ");
 			n = scan.next();
-			boolean bool = name.add(n);
+			boolean bool = name.add(n); //set은 중복이 안되니까
+										//중복이 되면 false가 나온다.
 			System.out.println(bool);
-			if(!bool) { //bool == false
+			if(!bool) { //bool == false (false가 나오면 true가 나오게 !반전시켜준다.)
+						//중복이 되면 존재하는 이름입니다가 출력되게 하는것.
 				System.out.println("존재하는 이름입니다.");
 			}else {
 				System.out.println("저장 완료");
